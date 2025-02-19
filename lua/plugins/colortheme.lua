577158f9ee941ff5a74f266be7cdcc92271b63a8
Brian Campisi Jr <bcampijr@gmail.com>
2025-02-19 00:37:38 -0800

    Adding some changes

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -8,6 +8,10 @@ return {
 		require("tokyonight").setup({
 			style = "night",
 			transparent = true,
+			-- Change the "hint" color to the "orange" color, and make the "error" color bright red
+			on_colors = function(colors)
+				--	colors.comment = "#ffffe600"
+			end,
 		})
 		vim.cmd([[colorscheme tokyonight]])
 	end,
